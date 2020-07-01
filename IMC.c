6587a8d7c75e8d7454ef7d3decc78174f8dd21da
cleibermarques <kleibermarques@hotmail.com>
2020-07-01 20:01:55 -0300

    Inclusão de msg informando a faixa de peso ideal para o paciente

--- IMC.c
+++ IMC.c
@@ -11,53 +11,59 @@ int main()
 	float peso;
 	float altura;
 	float imc;
+	float pesoIdealInicial;
+	float pesoIdealFinal;
 	char on;
 	char limpar;
 	char cls;
 	
 	do
 	{
-		printf("\r\n*****************************");
+		printf("\r\n\n*****************************");
 		printf("\r\n************ IMC ************");
-		printf("\r\n*****************************");
+		printf("\r\n********* Versão 1.5 ********");
 		printf("\r\n\nDigite o peso  (kg): ", peso);
 		scanf("%f", &peso);
 		printf("Digite a altura (m): ", altura);
 		scanf("%f", &altura);
 		imc = peso/(altura*altura);	
+		pesoIdealInicial = (18.50*(altura*altura));
+		pesoIdealFinal = (24.99* (altura*altura));
 		printf("\r\nO IMC é: %.2f ", imc);
-		
+				
 		if(imc > 0)
 		{
 			if(imc < 18.5)
 			{
-				printf("(Baixo peso)\n\n");
+				printf("(Baixo peso)\n");
 			}
 			if(imc >= 18.5 && imc < 25)
 			{
-				printf("(Eutrófico)\n\n");
+				printf("(Eutrófico)\n");
 			}
 			if(imc >= 25 && imc < 30)
 			{
-				printf("(Sobrepeso)\n\n");
+				printf("(Sobrepeso)\n");
 			}
 			if(imc >= 30 && imc < 35)
 			{
-				printf("(Obesidade Grau I)\n\n");
+				printf("(Obesidade Grau I)\n");
 			}
 			if(imc >= 35 && imc < 40)
 			{
-				printf("(Obesidade Grau II)\n\n");
+				printf("(Obesidade Grau II)\n");
 			}
 			if(imc >= 40)
 			{
-				printf("(Obesidade Grau III)\n\n");
+				printf("(Obesidade Grau III)\n");
 			}			
 		}
 		else
 		{
 			printf("\n\nIMC não pode ser menor ou igual a 0(zero)\n\n");
 		}
+		
+		printf("O peso ideal é de %.2f(kg) até %.2f(kg)\r\n", pesoIdealInicial, pesoIdealFinal);
 		
 		printf("\r\nDeseja continuar? [s/n] ");
 		scanf("\n%c", &on);
